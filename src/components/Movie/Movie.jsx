a120405442b7c252709d9e863c5be40f3f0311eb
Emil <emilaqvist@gmail.com>
2026-05-20 23:18:48 +0200

    Lade till bilder för stjärnor och ta bort
    Lade till bilderna från inlämning 3a för antalet stjärnor och för ta bort knapp som bild istället för knapp. Gjorde listan lite bättre visuellt också.
    
    Co-Authored-By: Elias Olsson <161301192+elleolsson@users.noreply.github.com>

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,11 +1,16 @@
 export default function Movie({ movie, index, removeMovie }) {
 
+    const ratingAmount = [];
+    for (let i = 0; i < movie.rating; i++) {
+        ratingAmount.push(<img key={i} src="../src/assets/star.png" alt="stjärna" />);
+    }
+
     return (
-        <li>
-            <p>{movie.title}</p>
-            <button className="btn btn-danger" onClick={() => removeMovie(index)}>
-                Ta Bort Film
-            </button>
+        <li className="list-group-item">
+            <hr />
+            <h3>{movie.title}</h3>
+            {ratingAmount}
+            <img src="../src/assets/delete.png" onClick={() => removeMovie(index)} />
         </li>
     );
 }
